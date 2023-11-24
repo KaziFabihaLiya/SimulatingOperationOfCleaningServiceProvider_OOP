91d80891694809eb9f8b7ffbd1f3ffe815faf680
Kazi Fabiha Golam Liya <Kazifabihaliya@gmail.com>
2023-11-24 22:18:13 +0600

    Liya

---
+++ src/KaziFabihaGolamLiya_2222156/AccountOfficer/Employee.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package KaziFabihaGolamLiya_2222156.AccountOfficer;
+
+/**
+ *
+ * @author kazif
+ */
+public class Employee {
+    Double 
+    
+}
