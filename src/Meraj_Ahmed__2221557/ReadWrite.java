13065fb67b42c7a6a79a8544f8ebfa2dfd1a1cac
ahmed <ahmed@DESKTOP-MA9JI4U>
2023-11-29 22:19:10 +0600

    Committed by @meraj0077

--- src/Meraj_Ahmed__2221557/ReadWrite.java
+++ src/Meraj_Ahmed__2221557/ReadWrite.java
@@ -84,7 +84,11 @@ public class ReadWrite {
         ObservableList<monthlyMaintenanceModel> monthlymaintainData = FXCollections.observableArrayList();
         ObservableList<qualityModel> qualityData = FXCollections.observableArrayList();
         ObservableList<jobRecruitModel> recruitData = FXCollections.observableArrayList();
+<<<<<<< HEAD
         ObservableList<Employee> empInfoData = FXCollections.observableArrayList();
+=======
+
+>>>>>>> origin/simulating_operations_of_chain_cleaning_service_provider
          
         
         
@@ -248,6 +252,7 @@ public class ReadWrite {
                 }
                 //  System.out.println("Meraj");               
             }
+<<<<<<< HEAD
             else if (instance instanceof Employee) {
                 f = new File(fileName);
                 fw = new FileInputStream(f);
@@ -261,6 +266,8 @@ public class ReadWrite {
                 }
                 //  System.out.println("Meraj");               
             }
+=======
+>>>>>>> origin/simulating_operations_of_chain_cleaning_service_provider
 
             
             
