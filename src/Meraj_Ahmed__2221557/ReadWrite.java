96f7dbb9142b29ce0eba94e44dc50cb0394fc4ba
meraj0077 <151558164+meraj0077@users.noreply.github.com>
2023-11-30 00:38:39 +0600

    Revert "Merge origin/simulating_operations_of_chain_cleaning_service_provider into simulating_operations_of_chain_cleaning_service_provider"
    This reverts commit 60530e2a5ecaa01a8800cfe1a842525a9443eb91, reversing
    changes made to 13065fb67b42c7a6a79a8544f8ebfa2dfd1a1cac.

--- src/Meraj_Ahmed__2221557/ReadWrite.java
+++ src/Meraj_Ahmed__2221557/ReadWrite.java
@@ -84,49 +84,435 @@ public class ReadWrite {
         ObservableList<monthlyMaintenanceModel> monthlymaintainData = FXCollections.observableArrayList();
         ObservableList<qualityModel> qualityData = FXCollections.observableArrayList();
         ObservableList<jobRecruitModel> recruitData = FXCollections.observableArrayList();
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Meraj_Ahmed__2221557;
+<<<<<<< HEAD
+        ObservableList<Employee> empInfoData = FXCollections.observableArrayList();
+=======
 
-import KaziFabihaGolamLiya_2222156.DummyMeaw.Request;
-import Meraj_Ahmed__2221557.Administration_Officer.bestEmployeeModel;
-import Meraj_Ahmed__2221557.Administration_Officer.developementModel;
-import Meraj_Ahmed__2221557.Administration_Officer.monthlyMaintenanceModel;
-import Meraj_Ahmed__2221557.Administration_Officer.qualityModel;
-import Meraj_Ahmed__2221557.Administration_Officer.sendNoticeModel;
-import Meraj_Ahmed__2221557.HR_Manager.HRLeaveModel;
-import Meraj_Ahmed__2221557.HR_Manager.jobRecruitModel;
-import Meraj_Ahmed__2221557.HR_Manager.timeAttendanceModel;
-import Meraj_Ahmed__2221557.HR_Manager.timePerformanceModel;
-import Meraj_Ahmed__2221557.HR_Manager.trainingModelClass;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+>>>>>>> origin/simulating_operations_of_chain_cleaning_service_provider
+         
+        
+        
+        
+        
+        
+        
+             
+ //Liya       
+        ObservableList<Request> requestData = FXCollections.observableArrayList();
+        
+        
+        
+        
+        
+        
+      
+ 
+ 
+        try {
+            if (instance instanceof SignupData) {
+                f = new File(fileName);
+                fw = new FileInputStream(f);
+                ois = new ObjectInputStream(fw);
+                try {
+                    while (true) {
+                        SData.add((SignupData) ois.readObject());
+                    }
+                } catch (Exception e) {
+                    System.out.println("Meraj signup exe");
+                }
+                //  System.out.println("Meraj");               
+            }
+            else if (instance instanceof trainingModelClass) {
+                f = new File(fileName);
+                fw = new FileInputStream(f);
+                ois = new ObjectInputStream(fw);
+                try {
+                    while (true) {
+                        tmcData.add((trainingModelClass) ois.readObject());
+                    }
+                } catch (Exception e) {
+                    System.out.println("Meraj trainingwork exe");
+                }
+                //  System.out.println("Meraj");               
+            }
+            else if (instance instanceof sendNoticeModel) {
+                f = new File(fileName);
+                fw = new FileInputStream(f);
+                ois = new ObjectInputStream(fw);
+                try {
+                    while (true) {
+                        noticeData.add((sendNoticeModel) ois.readObject());
+                    }
+                } catch (Exception e) {
+                    System.out.println("Meraj Send Notice exe");
+                }
+                //  System.out.println("Meraj");               
+            }
+            else if (instance instanceof HRLeaveModel) {
+                f = new File(fileName);
+                fw = new FileInputStream(f);
+                ois = new ObjectInputStream(fw);
+                try {
+                    while (true) {
+                        leaveData.add((HRLeaveModel) ois.readObject());
+                    }
+                } catch (Exception e) {
+                    System.out.println("Meraj Leave exe");
+                }
+                //  System.out.println("Meraj");               
+            }
+                        else if (instance instanceof timePerformanceModel) {
+                f = new File(fileName);
+                fw = new FileInputStream(f);
+                ois = new ObjectInputStream(fw);
+                try {
+                    while (true) {
+                        performanceData.add((timePerformanceModel) ois.readObject());
+                    }
+                } catch (Exception e) {
+                    System.out.println("Meraj Performance exe");
+                }
+                //  System.out.println("Meraj");               
+            }
+            else if (instance instanceof timeAttendanceModel) {
+                f = new File(fileName);
+                fw = new FileInputStream(f);
+                ois = new ObjectInputStream(fw);
+                try {
+                    while (true) {
+                        AttendanceData.add((timeAttendanceModel) ois.readObject());
+                    }
+                } catch (Exception e) {
+                    System.out.println("Meraj Attendance exe");
+                }
+                //  System.out.println("Meraj");               
+            }   
+            else if (instance instanceof bestEmployeeModel) {
+                f = new File(fileName);
+                fw = new FileInputStream(f);
+                ois = new ObjectInputStream(fw);
+                try {
+                    while (true) {
+                        bestempData.add((bestEmployeeModel) ois.readObject());
+                    }
+                } catch (Exception e) {
+                    System.out.println("Meraj Attendance exe");
+                }
+                //  System.out.println("Meraj");               
+            }
+            else if (instance instanceof developementModel) {
+                f = new File(fileName);
+                fw = new FileInputStream(f);
+                ois = new ObjectInputStream(fw);
+                try {
+                    while (true) {
+                        devplanData.add((developementModel) ois.readObject());
+                    }
+                } catch (Exception e) {
+                    System.out.println("Meraj devplan exe");
+                }
+                //  System.out.println("Meraj");               
+            }   
+            else if (instance instanceof monthlyMaintenanceModel) {
+                f = new File(fileName);
+                fw = new FileInputStream(f);
+                ois = new ObjectInputStream(fw);
+                try {
+                    while (true) {
+                        monthlymaintainData.add((monthlyMaintenanceModel) ois.readObject());
+                    }
+                } catch (Exception e) {
+                    System.out.println("Meraj maintennace exe");
+                }
+                //  System.out.println("Meraj");               
+            }
+            else if (instance instanceof qualityModel) {
+                f = new File(fileName);
+                fw = new FileInputStream(f);
+                ois = new ObjectInputStream(fw);
+                try {
+                    while (true) {
+                        qualityData.add((qualityModel) ois.readObject());
+                    }
+                } catch (Exception e) {
+                    System.out.println("Meraj quality exe");
+                }
+                //  System.out.println("Meraj");               
+            }
+            else if (instance instanceof jobRecruitModel) {
+                f = new File(fileName);
+                fw = new FileInputStream(f);
+                ois = new ObjectInputStream(fw);
+                try {
+                    while (true) {
+                        recruitData.add((jobRecruitModel) ois.readObject());
+                    }
+                } catch (Exception e) {
+                    System.out.println("Meraj recruit exe");
+                }
+                //  System.out.println("Meraj");               
+            }
+<<<<<<< HEAD
+            else if (instance instanceof Employee) {
+                f = new File(fileName);
+                fw = new FileInputStream(f);
+                ois = new ObjectInputStream(fw);
+                try {
+                    while (true) {
+                        empInfoData.add((Employee) ois.readObject());
+                    }
+                } catch (Exception e) {
+                    System.out.println("Meraj recruit exe");
+                }
+                //  System.out.println("Meraj");               
+            }
+=======
+>>>>>>> origin/simulating_operations_of_chain_cleaning_service_provider
 
-/**
- *
- * @author ahmed
- */
-public class ReadWrite {
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+//liya           
+            
+            else if (instance instanceof Request) {
+                f = new File(fileName);
+                fw = new FileInputStream(f);
+                ois = new ObjectInputStream(fw);
+                try {
+                    while (true) {
+                        requestData.add((Request) ois.readObject());
+                    }
+                } catch (Exception e) {
+                    System.out.println("Liya Req exe");
+                }              
+            }            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
 
-    public static <T> Boolean writeObjectToFile(String fileName, T instance) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+        }
+        
+            catch(Exception e){
+            System.out.println("Meraj False");
+        }
+            finally{
+            try {
+                if ( ois != null ){
+                    ois.close();
+                }
+            }
+            catch(IOException ex){
+                
+            }
+        }
+        if ( instance instanceof trainingModelClass ) return tmcData;
+        else if ( instance instanceof sendNoticeModel ) return noticeData;
+        else if ( instance instanceof HRLeaveModel ) return leaveData;
+        else if ( instance instanceof timePerformanceModel ) return performanceData;
+        else if ( instance instanceof timeAttendanceModel ) return AttendanceData;
+        else if ( instance instanceof bestEmployeeModel ) return bestempData;
+        else if ( instance instanceof developementModel ) return devplanData;
+        else if ( instance instanceof monthlyMaintenanceModel ) return monthlymaintainData;
+        else if ( instance instanceof qualityModel ) return qualityData;
+        else if ( instance instanceof jobRecruitModel ) return recruitData;
+        else if ( instance instanceof Employee ) return empInfoData;
+        
+        
+        
+        
+        
+        
+        
+        
+//liya        
+        else if ( instance instanceof Request ) return requestData;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        return SData;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public static <T> Boolean overWriteObjectToFile(String fileName, T instance){
         ObjectOutputStream oos = null;
         FileOutputStream fos = null;
         File f = null;
-        try {
+        try{
             f = new File(fileName);
-            if (f.exists()) {
-                fos = new FileOutputStream(f, true);
-                oos = new AppendableObjectOutputStream(fos);
-            } else {
+            if (f.exists() ){
                 fos = new FileOutputStream(f);
                 oos = new ObjectOutputStream(fos);
             }
@@ -134,44 +520,29 @@ public class ReadWrite {
                 oos.writeObject(instance);
                 oos.close();
                 return true;
-            } catch (IOException ex) {
-                GenerateAlerts.unsuccessfulAlert("Error while writing the File." + "\n"
-                        + "Please Check your Storage Efficiency, File type and name.");
             }
-        } catch (Exception e) {
-            GenerateAlerts.unsuccessfulAlert("Data is Vulnerable." + "\n" + "Please try again rechecking your data"
-                    + "If you can't solve the issue. Contact Software maintainer.");
-        } finally {
-            try {
-                if (oos != null) {
+            catch(IOException ex){
+                GenerateAlerts.unsuccessfulAlert("Error while writing the File." + "\n" +
+                                                "Please Check your Storage Efficiency, File type and name.");
+            }
+        }
+        catch (Exception e){
+            GenerateAlerts.unsuccessfulAlert("Data is Vulnerable." + "\n" + "Please try again rechecking your data" + 
+                                                "If you can't solve the issue. Contact Software maintainer.");
+        }
+        finally {
+            try{
+                if ( oos != null){
                     oos.close();
                 }
-            } catch (IOException ex) {
+            }
+            catch (IOException ex){
                 GenerateAlerts.unsuccessfulAlert("Error while closing the Binary File.");
                 return false;
             }
-        }
+        } 
         return false;
-
+        
     }
-
-    public static <T> ObservableList<?> readObjectToFile(String fileName, T instance) throws FileNotFoundException, IOException {
-        File f = null;
-        FileInputStream fw = null;
-        ObjectInputStream ois = null;
-        ObservableList<SignupData> SData = FXCollections.observableArrayList();
-        ObservableList<trainingModelClass> tmcData = FXCollections.observableArrayList();
-        ObservableList<sendNoticeModel> noticeData = FXCollections.observableArrayList();
-        ObservableList<HRLeaveModel> leaveData = FXCollections.observableArrayList();
-        ObservableList<timePerformanceModel> performanceData = FXCollections.observableArrayList();
-        ObservableList<timeAttendanceModel> AttendanceData = FXCollections.observableArrayList();
-        ObservableList<bestEmployeeModel> bestempData = FXCollections.observableArrayList();
-        ObservableList<developementModel> devplanData = FXCollections.observableArrayList();
-        ObservableList<monthlyMaintenanceModel> monthlymaintainData = FXCollections.observableArrayList();
-        ObservableList<qualityModel> qualityData = FXCollections.observableArrayList();
-        ObservableList<jobRecruitModel> recruitData = FXCollections.observableArrayList();
-
-        ObservableList<Employee> empInfoData = FXCollections.observableArrayList();
-=======
-
-
+}
+        
