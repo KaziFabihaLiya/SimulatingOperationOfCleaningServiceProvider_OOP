219de5f97dd41e3482efb5e765e11c9079d238da
Kazi Fabiha Golam Liya <Kazifabihaliya@gmail.com>
2023-11-30 15:20:12 +0600

    Liya

--- src/KaziFabihaGolamLiya_2222156/AccountOfficer/AccOfficerDashboardController.java
+++ src/KaziFabihaGolamLiya_2222156/AccountOfficer/AccOfficerDashboardController.java
@@ -14,6 +14,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 /**
@@ -22,6 +23,9 @@ import javafx.stage.Stage;
  * @author kazif
  */
 public class AccOfficerDashboardController implements Initializable {
+
+    @FXML
+    private BorderPane borderPaneId;
 
     /**
      * Initializes the controller class.
@@ -44,7 +48,15 @@ public class AccOfficerDashboardController implements Initializable {
     }
 
     @FXML
-    private void switchToProfileScalingButton(ActionEvent event) {
+    private void switchToProfileScalingButton(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("ProfitScaling.fxml"));
+        Scene TranScene = new Scene (root);
+        
+        Stage newWin = new Stage ();
+        
+        newWin.setScene(TranScene);
+        newWin.setTitle("Profit Statistic Panel");
+        newWin.show();
     }
 
     @FXML
@@ -61,6 +73,30 @@ public class AccOfficerDashboardController implements Initializable {
 
     @FXML
     private void switchToLoginSceneButton(ActionEvent event) {
+    }
+
+    @FXML
+    private void applyLeaveMenuItemOnClicked(ActionEvent event) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource("AcLeave.fxml"));
+        borderPaneId.setRight(parent);
+    }
+
+    @FXML
+    private void viewNoticeMenuItemOnCLlcked(ActionEvent event) throws IOException {
+        Parent fileChooserViewParent = FXMLLoader.load(getClass().getResource("AcViewNotice.fxml"));
+        Scene fileChooserViewScene = new Scene(fileChooserViewParent);
+        //Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Stage newWindow = new Stage();
+
+        newWindow.setTitle("Notice");
+        newWindow.setScene(fileChooserViewScene);
+        newWindow.show();
+    }
+
+    @FXML
+    private void logOutMenuItemOnClicked(ActionEvent event) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource("AcLogOut.fxml"));
+        borderPaneId.setBottom(parent);
     }
 
 }
---
+++ src/KaziFabihaGolamLiya_2222156/DummyMeaw/Request.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package KaziFabihaGolamLiya_2222156.DummyMeaw;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author kazif
+ */
+public class Request implements Serializable {
+    
+    String CompensationReason;
+    int amountReq;
+    String status;
+
+    public Request(String CompensationReason, int amountReq, String status) {
+        this.CompensationReason = CompensationReason;
+        this.amountReq = amountReq;
+        this.status = status;
+    }
+
+    public String getCompensationReason() {
+        return CompensationReason;
+    }
+
+    public void setCompensationReason(String CompensationReason) {
+        this.CompensationReason = CompensationReason;
+    }
+
+    public int getAmountReq() {
+        return amountReq;
+    }
+
+    public void setAmountReq(int amountReq) {
+        this.amountReq = amountReq;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Requesting for Compensation\b" + 
+                "\n Compensation Reason - " + CompensationReason + 
+                "\n Amount that needed to be paid :" + amountReq + ""
+                + "\n status " + status ;
+    }
+    
+    
+}
