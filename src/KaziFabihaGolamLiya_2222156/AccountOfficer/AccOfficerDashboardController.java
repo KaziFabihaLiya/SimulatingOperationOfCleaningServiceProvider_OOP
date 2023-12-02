1c1603a1dba8e7a05284c203d4544e4829465f18
Kazi Fabiha Golam Liya <Kazifabihaliya@gmail.com>
2023-12-02 14:27:32 +0600

    Liya

--- src/KaziFabihaGolamLiya_2222156/AccountOfficer/AccOfficerDashboardController.java
+++ src/KaziFabihaGolamLiya_2222156/AccountOfficer/AccOfficerDashboardController.java
@@ -14,6 +14,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -26,53 +28,39 @@ public class AccOfficerDashboardController implements Initializable {
 
     @FXML
     private BorderPane borderPaneId;
+    @FXML
+    private ImageView accountOfficerImageView;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        Image accountOfficerImage = new Image("/Icons/Account Officer.png");
+        accountOfficerImageView.setImage(accountOfficerImage);
+    }
+
     @FXML
     private void switchToTransactionButton(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("Transaction.fxml"));
-        Scene TranScene = new Scene (root);
-        
-        Stage newWin = new Stage ();
-        
+        Scene TranScene = new Scene(root);
+
+        Stage newWin = new Stage();
+
         newWin.setScene(TranScene);
         newWin.setTitle("Transaction Panel");
         newWin.show();
     }
 
-    @FXML
     private void switchToProfileScalingButton(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("ProfitScaling.fxml"));
-        Scene TranScene = new Scene (root);
-        
-        Stage newWin = new Stage ();
-        
+        Scene TranScene = new Scene(root);
+
+        Stage newWin = new Stage();
+
         newWin.setScene(TranScene);
         newWin.setTitle("Profit Statistic Panel");
         newWin.show();
-    }
-
-    @FXML
-    private void switchToVendorpanelButton(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("Vendor.fxml"));
-        Scene VendorScene = new Scene (root);
-        
-        Stage newWin = new Stage ();
-        
-        newWin.setScene(VendorScene);
-        newWin.setTitle("Vendor Panel");
-        newWin.show();
-    }
-
-    @FXML
-    private void switchToLoginSceneButton(ActionEvent event) {
     }
 
     @FXML
@@ -97,6 +85,30 @@ public class AccOfficerDashboardController implements Initializable {
     private void logOutMenuItemOnClicked(ActionEvent event) throws IOException {
         Parent parent = FXMLLoader.load(getClass().getResource("AcLogOut.fxml"));
         borderPaneId.setBottom(parent);
+    }
+
+    @FXML
+    private void switchToVendorMenuItemButton(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Vendor.fxml"));
+        Scene VendorScene = new Scene(root);
+
+        Stage newWin = new Stage();
+
+        newWin.setScene(VendorScene);
+        newWin.setTitle("Vendor Panel");
+        newWin.show();
+    }
+
+    @FXML
+    private void GoToCompensationMenuItemButton(ActionEvent event) {
+    }
+
+    @FXML
+    private void switchToProfileScalingMeniItemButton(ActionEvent event) {
+    }
+
+    @FXML
+    private void switchToVendorpanelButton(ActionEvent event) {
     }
 
 }
